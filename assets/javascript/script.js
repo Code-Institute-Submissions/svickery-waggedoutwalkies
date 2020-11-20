12e19f3d65c03ef6012abe60201f0e370060ad0f
svickery <s.vickery@hotmail.co.uk>
2020-11-20 12:46:30 +0000

    Card-text class given a padding of 5px so that it doesn't push completely to the edges.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,7 @@
 
 		$(document).ready(function () {
         $(".nav-link").on("click", function () {
-          $("a.nav-link").removeClass("active");
-          $(this).addClass("active");
+        $("a.nav-link").removeClass("active");
+        $(this).addClass("active");
         });
-      });
+    });
